eae7e5410fcea84a462821f3234d417a0410a2fd
MR <manolo_rod.98@hotmail.com>
2021-12-01 15:11:52 -0600

    Update crud2.php

--- Modelo(conexion)/crud2.php
+++ Modelo(conexion)/crud2.php
@@ -7,23 +7,16 @@ $conexion = $objeto->Conectar();
 // Recepcion de los datos enviados mediante el metodo POST desde el js
 $buscar = (isset($_POST['buscar'])) ? $_POST['buscar'] : '';
 $opcion = (isset($_POST['opcion'])) ? $_POST['opcion'] : '';
-$opt = (isset($_POST['opt'])) ? $_POST['opt'] : '';
 
-if($opcion == 1){  
-  $prueba = "SELECT * FROM lista_maestra WHERE numParte = '$buscar' ";
-  $resultado = $conexion->prepare($prueba);
-  $resultado->execute();
-  $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
-
-  /*
-  if($opt == 2){
-    $pruebaDos = "SELECT numParte FROM lista_maestra WHERE numParte = '$buscar' ";
-    $resultado = $conexion->prepare($pruebaDos);
+switch($opcion){
+  case 1:  
+    $prueba = "SELECT * FROM lista_maestra WHERE numParte = '$buscar' ";
+    $resultado = $conexion->prepare($prueba);
     $resultado->execute();
     $data = $resultado->fetchAll(PDO::FETCH_ASSOC);
-  
-  }
-  */
+
+  break;
+
 }
 //Enviar el array final en formato json a JS ---> DESCOMENTAR
 print json_encode($data);
