648aa972b3bf9da8b78f7a5565cfb27c6e7890f9
MR <manolo_rod.98@hotmail.com>
2021-12-01 15:09:11 -0600

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,7 +1,8 @@
+
 $(document).ready(function(){
+  // $(document).on("click", ".btnBuscar", function(){
   $("#btnBuscar").click(function(){
     opcion = 1;
-    opt = 2;
     buscar = $.trim($("#buscar").val()); // Numero de Parte.
 
     if(buscar != ''){
@@ -11,54 +12,76 @@ $(document).ready(function(){
         url: "Modelo(conexion)/crud2.php",
         type: "POST",
         dataType: "json",
-        data: {opcion: opcion, buscar: buscar, opt: opt,},
+        data: {opcion: opcion, buscar: buscar},
 
         success: function(data){
 
-          // OBTENER LOS VALORES DE LA SENTENCIA SQL Y GUARDAR EN CADA INPUT HTML
-          // UTILIZAR JSON.PARSE()
-          // VER MAS TUTORIALES
+          // MOSTRAR VALORES EN EL MODAL
+          numParte = $.trim($("#numParte").val(data[0].numParte));      
+          nomComp = $.trim($("#nomComp").val(data[0].nomComp));
+          provID_PC = $.trim($("#provID_PC").val(data[0].provID_PC));
+          cantCajas = $.trim($("#cantCajas").val(data[0].cantCajas));
+          cantXCajas = $.trim($("#cantXCajas").val(data[0].cantXCajas));
+          Total = $.trim($("#Total").val(data[0].Total));
+          provNombre = $.trim($("#provNombre").val(data[0].provNombre));
+          cantLote = $.trim($("#cantLote").val(data[0].cantLote));
+          cantCajasXTarima = $.trim($("#cantCajasXTarima").val(data[0].cantCajasXTarima));
+          
+          console.log(data);
 
-          swal("Datos enviados!!", "Presiona OK.", "success");
-
-          const arreglo = data;
-
-          const obj = JSON.parse(arreglo);
-
-          document.getElementById("nomComp");
-
-          /*
-          arr = data[0];
-          // console.log(data);
-
-          // console.log(data);
-          // console.log(arr);
-          valor = JSON.parse('{data}');
-          console.log(valor);
-          nomComp = $.trim($("#nomComp").val(valor));  
-          */
         }
       });
-
-      numParte = $.trim($("#numParte").val(buscar));
-
       // Estilos CSS
       $(".modal-header").css("background-color", "#85929E");
       $(".modal-header").css("color", "white");
       $(".modal-title").text("Control de Lotes");
       $("#checkIn").modal("show");
-      $("#checkIn").modal("hide");
+      // $("#checkIn").modal("hide");
       
     }else{swal("Campo vacio!", "Ingrese el numero de parte nuevamente", "error");}
+    
+    
   });
+
+  $("#formCheck").submit(function(e){ // ENVIO DE PARAMETROS
+    e.preventDefault();
+    // DATOS DEL NUMERO DE PARTE.
+    numParte = $.trim($("#numParte").val(numParte));
+    nomComp = $.trim($("#nomComp").val());
+    provID_PC = $.trim($("#provID_PC").val());
+    cantCajas = $.trim($("#cantCajas").val());
+    cantXCajas = $.trim($("#cantXCajas").val());
+    Total = $.trim($("#Total").val());
+    provNombre = $.trim($("#provNombre").val());
+    cantLote = $.trim($("#cantLote").val());
+    cantCajasXTarima = $.trim($("#cantCajasXTarima").val());
+
+    opt = 2; // key para enviar datos a lista_registro.
+
+    $.ajax({
+      url: "Modelo(conexion)/process.php",
+      type: "POST",
+      dataType: "json",
+      data: {numParte: numParte, nomComp: nomComp, provID_PC: provID_PC, cantCajas: cantCajas, cantXCajas: cantXCajas, Total: Total, provNombre: provNombre, cantLote: cantLote, cantCajasXTarima: cantCajasXTarima, opt: opt},
+
+      success: function(data){
+        swal("Datos enviados!", "Pulsa OK.", "success");
+        console.log(data);
+
+      }
+    });
+  });
+  $("#checkIn").modal("hide"); // CERRAR MODAL.
 });
 
+/*
+// DATA TABLE
 function mostrarDatos(){
   $('#tablaEmpleados').DataTable({
 
   });
 }
-
+*/
 
 
   
