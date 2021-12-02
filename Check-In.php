b377d369f53a971ac4dc72c3ae7b45563d6c072d
MR <manolo_rod.98@hotmail.com>
2021-12-01 23:33:49 -0600

    Update Check-In.php

--- Check-In.php
+++ Check-In.php
@@ -66,13 +66,15 @@
             </div>
 
             <!-- Nav Item - Pages Collapse Menu -->
+
+            
             <li class="nav-item active">
                 <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true"
                     aria-controls="collapseTwo">
                     <i class="fas fa-fw fa-cog"></i>
                     <span>Productividad</span>
                 </a>
-                <div id="collapseTwo" class="collapse show" aria-labelledby="headingTwo"
+                <div id="collapseTwo" class="collapse hide" aria-labelledby="headingTwo"
                     data-parent="#accordionSidebar">
                     <div class="bg-white py-2 collapse-inner rounded">
                         <h6 class="collapse-header">Opciones:</h6>
