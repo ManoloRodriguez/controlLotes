685d0bad49e07db549448df3a86ab5e231425097
MR <manolo_rod.98@hotmail.com>
2021-12-17 15:17:14 -0600

    Update registros.js

--- registros.js
+++ registros.js
@@ -1,8 +1,64 @@
 $(document).ready(function(){
-  controlLotes = $("#controlLotes").DataTable({ // cambiar controlLotes por tablaEmpleados.
-    scrollY: '50vh',
-    scrollCollapse: true,
+  $("#btnBuscar").click(function(){
+    numParte = $("#numParte").val();
+    nomComp = $("#nomComp").val();
+    provNombre = $("#provNombre").val();
 
+    // VERIFICAR CAMPOS VACIOS DE FILTRO.
+    if (numParte != '' || nomComp != '' || provNombre != ''){
+      $.ajax({
+        url: "Modelo(conexion)/registrosFiltro.php",
+        type: "POST",
+        dataType: "json",
+        data:{
+          numParte: numParte,
+          nomComp: nomComp,
+          provNombre: provNombre
+
+        },
+        
+        success: function(data){
+          console.log(data);
+          
+          registroFiltro = $("#registroFiltro").DataTable({ // cambiar controlLotes por tablaEmpleados.
+            "scrollX": true,
+            "scrollY": "200px",
+            "scrollCollapse": true,
+            "paging": false,
+            // Para cambiar el lenguaje a español
+            "language":{
+              "lengthMenu": "Mostrar _MENU_ registros",
+              "zeroRecords": "No se encontraron resultados",
+              "info": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+              "infoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+              "infoFiltered": "(filtrado de un total de _MAX_ registros)",
+              "sSearch": "Buscar:",
+              "oPaginate":{
+                "sFirst": "Primero",
+                "sLast": "Ultimo",
+                "sNext": "Siguiente",
+                "sPrevious": "Anterior"
+              },
+            
+              "sProcessing": "Procesando...",
+          
+            }
+          });
+        }
+      });
+  
+    }else{
+      swal("Campo vacio!", "Ingrese el DensoLot nuevamente", "error");
+
+    }
+  });
+  
+  /*
+  registroFiltro = $("#registroFiltro").DataTable({ // cambiar controlLotes por tablaEmpleados.    
+    "scrollX": true,
+    "scrollY": "200px",
+    "scrollCollapse": true,
+    "paging": false,
     // Para cambiar el lenguaje a español
     "language":{
       "lengthMenu": "Mostrar _MENU_ registros",
@@ -17,8 +73,11 @@ $(document).ready(function(){
         "sNext": "Siguiente",
         "sPrevious": "Anterior"
       },
+    
       "sProcessing": "Procesando...",
-
+  
     }
   });
+  */
+ 
 });
